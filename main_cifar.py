4039d161c5d22214bbd019be2cf615863fd17c80
orris27 <gsorris27@gmail.com>
2020-01-26 15:57:03 +0800

    change dataset root

--- main_cifar.py
+++ main_cifar.py
@@ -16,7 +16,7 @@ def main_cifar(model='PredNetBpD', circles=5, gpunum=1, Tied=False, weightDecay=
     use_cuda = True # torch.cuda.is_available()
     best_acc = 0  # best test accuracy
     start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-    batchsize = 16
+    batchsize = 128
     root = './'
     rep = 1
     lr = 0.01
@@ -46,9 +46,9 @@ def main_cifar(model='PredNetBpD', circles=5, gpunum=1, Tied=False, weightDecay=
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
     data_dir = '../../datasets/torchvision'
-    trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=True, transform=transform_train)
+    trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=False, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchsize, shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=True, transform=transform_test)
+    testset = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=False, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=False, num_workers=2)
     
     # Model
