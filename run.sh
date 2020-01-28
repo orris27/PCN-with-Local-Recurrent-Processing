3f96d3e9aa04619b7f23ebd663c83b3bce4044d8
orris27 <40049332+orris27@users.noreply.github.com>
2020-01-28 22:28:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python main.py --batch_size 128 --circles 1 --backend modelC --dataset_name cifar10 --adaptive 1
+python main.py --batch_size 128 --circles 2 --backend modelC --dataset_name cifar100 --adaptive 1 --max_epoch 100
 
 # python main_cifar.py
