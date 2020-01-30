718c232cdc87be93e023e3fa2f8212160968cee5
orris27 <gsorris27@gmail.com>
2020-01-30 23:53:44 +0800

    change run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-python main.py --batch_size 128 --circles 1 --backend modelC --dataset_name cifar10 --adaptive 1 --max_epoch 300 --dropout 0.5 --avg 1  >> cir1_adaptive_dropout0.5_avg.log
-python main.py --batch_size 128 --circles 1 --backend modelC --dataset_name cifar10 --adaptive 0 --max_epoch 300 --dropout 0.5 --avg 1  >> cir1_dropout0.5_avg.log
-python main.py --batch_size 128 --circles 0 --backend modelC --dataset_name cifar10 --max_epoch 300 --dropout 0.5 --avg 1  >> cir0_dropout0.5_avg.log
+python main.py --batch_size 128 --circles 1 --backend modelC --dataset_name cifar10 --adaptive 1 --max_epoch 300 --dropout 0.5 --avg 0 --step_all 15 --step_clf 10
 
 # python main_cifar.py --batch_size 128 --circles 1
